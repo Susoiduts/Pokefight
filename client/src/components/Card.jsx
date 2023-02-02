ee85830d19f6cb2147456704ec8c1c434a8de8c9
Henning Spier <henning.spier@gmail.com>
2023-02-02 16:13:52 +0100

    mit cards

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Card = () => {
+  return (
+    <>
+        <div className="card">
+            <h2>1</h2>
+            <img src="../logo192.png" alt="" />
+            <h2>Charmander</h2>
+        </div>
+    
+    </>
+  )
+}
+
+export default Card
---
+++ client/src/components/Main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Card from './Card'
+import Pokeinfo from "./Pokeinfo"
+
+const Main = () => {
+  return (
+    <>
+    <div className="container">
+        <div className="left-content">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+
+        <div className="right-content">
+            <Pokeinfo />
+        </div>
+
+        
+    </div>
+
+    </>
+  )
+}
+
+export default Main
