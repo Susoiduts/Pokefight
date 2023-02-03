0253689efe0a6f062269313a98ef865e4e2cad03
test <tobias.wedhorn@gmx.de>
2023-02-03 17:17:03 +0100

    Deleted Backend files

--- routes/game.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { saveResult, getScorelist } = require("../controllers/game");
-
-router.route("/game/save").get(saveResult);
-
-router.route("/game/leaderboard").get(getScorelist);
-
-module.exports = router;
